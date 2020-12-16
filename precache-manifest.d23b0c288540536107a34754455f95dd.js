eccd041027b27b2a7eab3432591867a135ae150f
ogawa_mitsunori <mitsunori.ogawa@drecom.co.jp>
2020-12-16 12:30:03 +0900

    first commit.

---
+++ precache-manifest.d23b0c288540536107a34754455f95dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072e723fa04463ce056427397de8005f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83d1b8ae9ee3062be845",
+    "url": "/static/css/main.a3d3e8c8.chunk.css"
+  },
+  {
+    "revision": "a8cdd13041333c83e356",
+    "url": "/static/js/2.9ee045f1.chunk.js"
+  },
+  {
+    "revision": "83d1b8ae9ee3062be845",
+    "url": "/static/js/main.2b353c82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01caf469bfb18343074bdae346446512",
+    "url": "/static/media/logo_footer.01caf469.png"
+  },
+  {
+    "revision": "01caf469bfb18343074bdae346446512",
+    "url": "/static/media/logo_small.01caf469.png"
+  }
+]);
